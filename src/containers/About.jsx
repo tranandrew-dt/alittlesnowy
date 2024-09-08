9bb2fd2108ce806c659f0d4979f9bc30bf22d8f1
Andrew D Tran <tranandrew.dt@gmail.com>
2024-09-08 15:05:39 -0700

    [content] Added Hobbies to About container's content; created a NavBar

---
+++ src/containers/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>My Hobbies</h1>
+      <ol>
+        <li>Coding My Random Ideas</li>
+        <li>Bodybuilding/Weightlifting</li>
+        <li>Gaming (Love me some RPGs)</li>
+        <li>Going on Food Adventures (Foodventures)</li>
+        <li>Cooking</li>
+        <li>Concerts</li>
+      </ol>
+      <br />
+      <hr size="2" />
+      <h3>
+        <a href="index.html">Return to Home</a>
+      </h3>
+    </div>
+  );
+}
+
+export default About;
