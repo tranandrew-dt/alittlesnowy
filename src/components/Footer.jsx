6fa530f4457a03dda57ba5d105c946c2f3403946
Andrew D Tran <tranandrew.dt@gmail.com>
2024-09-08 14:58:05 -0700

    [content] Building out HomePage container's content and starting on Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      {" "}
+      <a href="https://www.linkedin.com/in/tranandrewdt/">
+        Connect with me on LinkedIn!
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
