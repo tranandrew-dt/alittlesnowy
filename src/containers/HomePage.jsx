a9e29f29603e087d5396ab76dce15752a57962eb
Andrew D Tran <tranandrew.dt@gmail.com>
2024-09-08 17:12:03 -0700

    [style:HomePage.css] updated styling so it's mobile friendly

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -16,19 +16,16 @@ function HomePage() {
           />
           <div className="profile-info">
             <h1>Andrew D. Tran</h1>
-            <p>
-              <h2>
-                <em>Software Engineer (Full-Stack)</em>
-              </h2>
-            </p>
+            <h2>
+              <em>Software Engineer (Full-Stack)</em>
+            </h2>
             <p>
               Hello! I'm a software engineer with 4 years of full stack
               development experience specializing in Java, React.js, and PL/SQL.
               I'm dedicated to delivering efficient, user-centered solutions
               that improve operational effectiveness and enhance overall user
-              experience. With that being said, I have proven ability to
-              collaborate across teams and integrate third-party systems to
-              streamline complex processes.
+              experience. I have a proven ability to collaborate across teams
+              and integrate third-party systems to streamline complex processes.
             </p>
           </div>
         </div>
